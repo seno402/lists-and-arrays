75c866e0a9f6a9e9fe9fe053a7d080728d433d21
seno402 <reallbob2020@gmail.com>
2026-04-16 12:13:31 +0100

    Add files via upload

---
+++ ac4.py
@@ -0,0 +1,9 @@
+import array as arr  
+array_num = arr.array ('i',[1,3,5,3,7,9,3])
+print("original array" +str(array_num))
+
+print("number of occurrences of the number 3 in the array" +str(array_num.count(3)))
+
+array_num.reverse()
+print("reverse the order of the items")
+print(str(array_num))
